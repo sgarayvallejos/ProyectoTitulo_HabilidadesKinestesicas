1677bf25a4b1fac90498766a8ab8e432380c2198
DESKTOP-HFMT8OG\SAG <sebastiangarayv@gmail.com>
2019-04-25 03:13:15 -0400

    cambios
    cambios en el juego

---
+++ Assets/scripts/selectgirl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectgirl : MonoBehaviour
+{
+    public GameObject playerBoy, playegirl;
+    // Start is called before the first frame update
+
+    GameObject play1, play2;
+
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
